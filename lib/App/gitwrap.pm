57b18221b37cab79c48966b9112d22e514e3a151
perlancar (@netbook-zenbook-ux305) <perlancar@gmail.com>
2016-06-10 17:37:07 +0700

    Initial commit

---
+++ lib/App/gitwrap.pm
@@ -0,0 +1,19 @@
+package App::gitwrap;
+
+# DATE
+# VERSION
+
+1;
+#ABSTRACT: Git wrapper script
+
+=head1 SYNOPSIS
+
+See the included script L<gitwrap>.
+
+
+=head1 SEE ALSO
+
+C<git> command
+
+L<Git::Wrapper> - similar name but this wraps the C<git> command so it can be
+used like a Perl library.
